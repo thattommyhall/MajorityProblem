c25c2280d5d4d92a30ab555bf8a44bdf2d227039
thattommyhall <thattommyhall@gmail.com>
2013-07-01 01:34:49 +0100

    remove + and * on fitness

--- src/worker/ca_vote/worker.cljs
+++ src/worker/ca_vote/worker.cljs
@@ -10,11 +10,11 @@
 (defn process-sample [sample]
   (doseq [genome sample]
     (let [fitness (sim/fitness genome)]
-      (POST (+ "/results2/" genome "/" fitness) "")
+      (POST (+ "/results3/" genome "/" fitness) "")
       (js/setTimeout run 0))))
 
 (defn ^:export run [] 
-  (GET "/sample2" (fn [sample]
+  (GET "/sample3" (fn [sample]
                    (process-sample (.parse js/JSON sample)))))
 
 
