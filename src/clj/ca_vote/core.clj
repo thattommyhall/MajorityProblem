19db2864ec80246c6ddd0ca5a1ad38e2304cdd95
thattommyhall <thattommyhall@gmail.com>
2013-07-02 23:04:45 +0100

    add analytics

--- src/clj/ca_vote/core.clj
+++ src/clj/ca_vote/core.clj
@@ -13,6 +13,13 @@
   (let [onload "ca_vote.display.draw();"]
     (html [:head {:title env}                           
            [:script {:src (str "js/" env ".js")}]
+           [:script "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+                     ga('create', 'UA-42209406-1', 'thattommyhall.com');
+                     ga('send', 'pageview');"]
            [:link {:rel "stylesheet" :href "css/style.css"}]]
           [:body {:onload onload}
            [:canvas#voting {:width "800" :height "800"}]
